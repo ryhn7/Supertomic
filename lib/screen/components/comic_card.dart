c7738f37b91f92f25985604a6a2db1a280a1c92c
Rayhan Illyas <rayhan205732@gmail.com>
2023-11-13 00:21:12 +0700

    feat(homescreen): fix comic cards v1

--- lib/screen/components/comic_card.dart
+++ lib/screen/components/comic_card.dart
@@ -8,41 +8,52 @@ class ComicCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(16),
-          child: Image.asset(comicData.coverImage,
-              height: 300),
-        ),
-        const SizedBox(height: 12),
-        Text(
-          comicData.title,
-          style: getComicBookRegular14(color: Colors.black),
-        ),
-        const SizedBox(height: 4),
-        Text(
-          comicData.author,
-          style: getComicBookRegular14(color: Colors.grey),
-        ),
-        const SizedBox(height: 8),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Row(children: [
-              Image.asset('assets/images/star.png', width: 20, height: 20),
-              const SizedBox(width: 4),
-              Text(
-                comicData.rating.toString(),
-                style: getComicBookRegular14(color: Colors.black),
-              ),
-            ]),
-            Image.asset(comicData.publisherImage, width: 40, height: 50),
-          ],
-        )
-      ],
+    return ConstrainedBox(
+      constraints: const BoxConstraints(maxWidth: 206),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(16),
+            child: Image.asset(comicData.coverImage, height: 300),
+          ),
+          const SizedBox(height: 12),
+          ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 190),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  comicData.title,
+                  overflow: TextOverflow.ellipsis,
+                  style: getComicBookRegular14(color: Colors.black),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  comicData.author,
+                  style: getComicBookRegular14(color: Colors.grey),
+                ),
+                const SizedBox(height: 8),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(children: [
+                Image.asset('assets/images/star.png', width: 20, height: 20),
+                const SizedBox(width: 4),
+                Text(
+                  comicData.rating.toString(),
+                  style: getComicBookRegular14(color: Colors.black),
+                ),
+              ]),
+              Image.asset(comicData.publisherImage, width: 40, height: 50),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
