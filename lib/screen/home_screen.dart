7fe8d25fe69dc4f3d3f46dc089a87a10ad81a98b
Rayhan Illyas <rayhan205732@gmail.com>
2023-11-13 02:05:07 +0700

    feat(homescreen): wip heroes card

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:supertomic/data/model/comic_model.dart';
+import 'package:supertomic/data/model/hero_model.dart';
 import 'package:supertomic/screen/widgets/comic_card.dart';
+import 'package:supertomic/screen/widgets/heroes_card.dart';
 import 'package:supertomic/theme/style.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,58 +10,133 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget header() {
+      return Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              color: Colors.deepPurple,
+            ),
+            height: 356,
+          ),
+          Image.asset('assets/images/heroes/batman_cover.png'),
+        ],
+      );
+    }
+
+    Widget heroesSection() {
+      return Container(
+        margin: const EdgeInsets.only(top: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 16),
+              child: Text('YOUR FAVOURITE HEROES',
+                  style: getGeneraBoldStyle20(color: Colors.black)),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              children: [
+                const SizedBox(width: 16),
+                HeroesCard(),
+                const SizedBox(width: 16),
+                HeroesCard(),
+                const SizedBox(width: 16),
+                HeroesCard(),
+                const SizedBox(width: 16),
+              ],
+            ),
+        
+            // HeroesCard(),
+            // ListView.builder(
+            //       padding: const EdgeInsets.only(left: 0),
+            //       itemCount: heroList.length,
+            //       scrollDirection: Axis.horizontal,
+            //       itemBuilder: (context, index) {
+            //         final HeroModel heroData = heroList[index];
+            //         final bool isLastItem = index == heroList.length - 1;
+            //         return SizedBox(
+            //           child: Container(
+            //             margin: EdgeInsets.only(
+            //               left: index == 0 ? 16 : 0,
+            //               right: isLastItem ? 16 : 20,
+            //             ),
+            //             child: InkWell(
+            //               onTap: () {},
+            //               child: HeroesCard(heroData),
+            //             ),
+            //           ),
+            //         );
+            //       },
+            //     )
+            // ListView(
+            //   scrollDirection: Axis.horizontal,
+            //   children: [
+            //     // loop 10 times
+            //     for (var i = 0; i < 10; i++)
+            //       Container(
+            //         margin: const EdgeInsets.only(right: 20),
+            //         child: const SizedBox(
+            //           width: 206,
+            //           child: HeroesCard(),
+            //         ),
+            //       )
+            //   ],
+            // ),
+          ],
+        ),
+      );
+    }
+
+    Widget comicSection() {
+      return Container(
+        margin: const EdgeInsets.only(top: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 16),
+              child: Text('TOP RATED COMICS',
+                  style: getGeneraBoldStyle20(color: Colors.black)),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+                height: 430,
+                child: ListView.builder(
+                  padding: const EdgeInsets.only(left: 0),
+                  itemCount: comicModelList.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    final ComicModel comicData = comicModelList[index];
+                    final bool isLastItem = index == comicModelList.length - 1;
+                    return SizedBox(
+                      child: Container(
+                        margin: EdgeInsets.only(
+                          left: index == 0 ? 16 : 0,
+                          right: isLastItem ? 16 : 20,
+                        ),
+                        child: InkWell(
+                          onTap: () {},
+                          child: ComicCard(comicData),
+                        ),
+                      ),
+                    );
+                  },
+                ))
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Stack(
-              children: [
-                Container(
-                  decoration: const BoxDecoration(
-                    color: Colors.deepPurple,
-                  ),
-                  height: 356,
-                ),
-                Image.asset('assets/images/heroes/batman_cover.png'),
-              ],
-            ),
-            const SizedBox(height: 20),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 16),
-                  child: Text('TOP RATED COMICS',
-                      style: getGeneraBoldStyle20(color: Colors.black)),
-                ),
-                const SizedBox(height: 20),
-                SizedBox(
-                    height: 430,
-                    child: ListView.builder(
-                      padding: const EdgeInsets.only(left: 0),
-                      itemCount: comicModelList.length,
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (context, index) {
-                        final ComicModel comicData = comicModelList[index];
-                        final bool isLastItem =
-                            index == comicModelList.length - 1;
-                        return SizedBox(
-                          child: Container(
-                            margin: EdgeInsets.only(
-                              left: index == 0 ? 16 : 0,
-                              right: isLastItem ? 16 : 20,
-                            ),
-                            child: InkWell(
-                              onTap: () {},
-                              child: ComicCard(comicData),
-                            ),
-                          ),
-                        );
-                      },
-                    ))
-              ],
-            )
+            header(),
+            // comicSection(),
+            heroesSection(),
           ],
         ),
       ),
--- lib/screen/widgets/heroes_card.dart
+++ lib/screen/widgets/heroes_card.dart
@@ -1,56 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:supertomic/theme/style.dart';
+import 'package:supertomic/data/model/hero_model.dart';
+import 'package:supertomic/theme/colors.dart';
 
 class HeroesCard extends StatelessWidget {
+  // final HeroModel heroData;
   const HeroesCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ConstrainedBox(
-      constraints: const BoxConstraints(maxWidth: 206),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(16),
-            child: Image.asset('assets/images/comics/detective_comics', height: 300),
-          ),
-          const SizedBox(height: 12),
-          ConstrainedBox(
-            constraints: const BoxConstraints(maxWidth: 190),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "comicData.title",
-                  overflow: TextOverflow.ellipsis,
-                  style: getComicBookRegular14(color: Colors.black),
-                ),
-                const SizedBox(height: 4),
-                Text(
-                  "comicData.author",
-                  style: getComicBookRegular14(color: Colors.grey),
-                ),
-                const SizedBox(height: 8),
-              ],
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(children: [
-                Image.asset('assets/images/star.png', width: 20, height: 20),
-                const SizedBox(width: 4),
-                Text(
-                  "4.8",
-                  style: getComicBookRegular14(color: Colors.black),
-                ),
-              ]),
-              Image.asset("assets/images/dc_logo", width: 40, height: 50),
-            ],
-          )
-        ],
+    const double avatarRadius = 50;
+
+    return Container(
+      width: avatarRadius * 2, // Diameter of the circle
+      height: avatarRadius * 2,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: lust,
+      ),
+      child: OverflowBox(
+        minWidth: 0.0,
+        minHeight: 0.0,
+        maxWidth: double.infinity,
+        maxHeight: double.infinity,
+        child: Image.asset(
+          'assets/images/heroes/spiderman.png',
+          width: 130,
+          height: 130,
+        ),
       ),
     );
   }
