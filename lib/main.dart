1a95c172b7a6fc8d684e0fd7bf76318e11ad0728
Rayhan Illyas <rayhan205732@gmail.com>
2023-11-13 08:52:21 +0700

    refactor: adjust code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:supertomic/screen/widgets/comic_card.dart';
 import 'package:supertomic/screen/home_screen.dart';
 
 void main() {
@@ -13,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Supertomic',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
